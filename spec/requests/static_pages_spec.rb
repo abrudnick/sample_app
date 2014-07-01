f42a8bedf993fd04efc8e2c8ce4d93ad27c218a9
Adam Brudnick <abrudnick@wesleyan.edu>
2014-06-30 22:52:40 -0400

    Finished with pseudo-dynamic proramming

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,8 +7,11 @@ describe "Static pages" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
+    end
     
-    
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
     end
   end
   
@@ -17,7 +20,12 @@ describe "Static pages" do
     
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_content('Help')
+      expect(page).to have_content("Help")
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
   
@@ -26,6 +34,11 @@ describe "Static pages" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title('About')
     end
   end
 end
